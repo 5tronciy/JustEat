d61e38c90f67906da06ca76e684218638652415e
Roman Golchuk <rgolchuk@masslight.com>
2023-05-13 12:11:30 +0200

    fix: prevent custom Image for svgs

--- src/components/Advantages/AdvantageCard/AdvantageCard.js
+++ src/components/Advantages/AdvantageCard/AdvantageCard.js
@@ -1,11 +1,10 @@
 import React from "react";
-import Image from "../../common/Image";
 import "./AdvantageCard.scss";
 
 const AdvantageCard = ({ icon, title, description }) => {
   return (
     <div className="advantage">
-      <Image src={icon} alt="" className="advantage__icon" />
+      <img src={icon} alt="" className="advantage__icon" />
       <div className="advantage__title">{title}</div>
       <div className="advantage__description">{description}</div>
     </div>
